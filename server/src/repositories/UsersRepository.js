8faa2730e25c1f47146b69d92314a0b7b438af23
paulskaufmanis <71510131+paulskaufmanis@users.noreply.github.com>
2021-08-20 08:52:40 +0300

    Users api backend

--- server/src/repositories/UsersRepository.js
+++ server/src/repositories/UsersRepository.js
@@ -1,18 +1,49 @@
 const User = require("../models/User");
-const data = require("../data/USERS.json");
+const receivedData = require("../data/Users.json");
+const bcrypt = require("bcrypt");
 
 class UsersRepository {
-  constructor(user) {
-    this.user = user;
+  data = [];
+
+  constructor() {
+    receivedData.forEach((item) => {
+      this.data.push(item);
+    });
   }
 
-  getAllUsers() {}
+  getAllUsers() {
+    return this.data.map((data) => {
+      const userModel = new User(data);
+      return userModel.getData();
+    });
+  }
 
-  getUserById() {}
+  async findUser(data) {
+    return this.data.find((user) => user.username === data);
+  }
 
-  createUser() {}
+  getSingleUser(id) {
+    let found;
+    this.data.map((item) => (item.id == id ? (found = item) : "not found"));
+    return found;
+  }
 
-  deleteUser() {}
+  addUser(data) {
+    // model used to validate and parse data
+
+    const userModel = new User(data);
+    const dataToStore = userModel.getData();
+    const id = Date.now();
+    const storedData = Object.assign({}, dataToStore, { id });
+    this.data.push(storedData);
+    return userModel.getData();
+  }
+
+  deleteUser(id) {
+    let found;
+    this.data.map((item) => (item.id == id ? (found = item) : "not found"));
+    return this.data.splice(this.data.indexOf(found), 1);
+  }
 }
 
-module.exports = UsersRepository;
+module.exports = new UsersRepository();
