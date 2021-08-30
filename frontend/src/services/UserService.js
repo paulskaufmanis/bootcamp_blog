8bf6add19962973eb21866bdb3c9e2c655f3b724
paulskaufmanis <71510131+paulskaufmanis@users.noreply.github.com>
2021-08-30 19:37:22 +0300

    step forward with tokens

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -1,4 +1,5 @@
 const url = "http://localhost:3300/api/users-management/users/";
+import axios from "axios";
 
 class UserService {
   static getUsers(users, method = "GET") {
@@ -28,24 +29,47 @@ class UserService {
       .then((data) => console.log("User added!", data));
   }
 
-  static async loginUser(user) {
-    await fetch(`${url}login`, {
-      method: "POST",
-      headers: {
-        // check if token, pass to header
-        "Content-Type": "application/json",
-      },
-      // credentials: "include",
-      body: JSON.stringify(user),
-    })
-      //   .then(console.log(JSON.stringify(user)))
-      .then((res) => res)
-
-      .then((data) => {
-        console.log(JSON.stringify(data.headers), data.headers);
-        // data is token, store token to local storage
+  static loginUser(user) {
+    axios
+      .post(`${url}${"login"}`, {
+        username: user.username,
+        password: user.password,
+      })
+      .then((response) => {
+        console.log(response.data);
+        localStorage.setItem("token", response.data.accessToken);
       });
+    console.log(localStorage.token);
   }
+
+  //____________________________________________________________________________________________________
+
+  // static async loginUser(user) {
+  //   await fetch(`${url}login`, {
+  //     method: "POST",
+  //     headers: {
+  //       "x-access-token": "adgdfg sdhgsdfg sdfg sdfg asgd",
+  //       "Content-Type": "application/json",
+  //       // check if token, pass to header
+  //       // "Access-Control-Allow-Credentials": true,
+  //       // authorization: "Bearer ",
+  //     },
+  //     // credentials: "include",
+  //     body: JSON.stringify(user),
+  //   })
+  //     //   .then(console.log(JSON.stringify(user)))
+  //     // .then((res) => res)
+  //     .then((res) => {
+  //       console.log(res.data);
+  //       localStorage.setItem("token", res.token);
+  //     })
+  //     .then((data) => {
+  //       console.log(data);
+  //       // data is token, store token to local storage
+  //     });
+  // }
+
+  //____________________________________________________________________________________________________
 
   static deleteUser(id, method = "DELETE") {
     fetch(`${url}${id}`, { method }).then((response) => console.log(response));
